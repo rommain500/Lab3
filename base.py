a69590db3c4a4717aa3b1522d7ed1bfb380c65b5
XfortunaX <buklin.sergei@yandex.ru>
2016-10-13 20:10:34 +0300

    Add files via upload

---
+++ base.py
@@ -0,0 +1,35 @@
+import requests
+
+class BaseClient:
+    BASE_URL = "https://api.vk.com/method/"
+
+    method = None
+    http_method = None
+
+    def get_params(self):
+        pass
+
+    def get_json(self):
+        pass
+
+    def get_headers(self):
+        pass
+
+    def generate_url(self, method):
+        return '{0}{1}'.format(self.BASE_URL, method)
+
+    def _get_data(self, method, http_method):
+        response = None
+
+        response = requests.get(self.generate_url(method), params=self.get_params())
+
+        return self.response_handler(response)
+
+    def response_handler(self, response):
+        return response
+
+    def execute(self):
+        return self._get_data(
+            self.method,
+            http_method=self.http_method
+        )
